0c7dc52e4f3c11f75af84531e7bb6042ece69841
xabi2020 <xfuentes@hotmail.com>
2020-10-16 10:09:25 +0200

    Refactor. Quitamos test y codigo innecesario

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,6 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders "Hello World!" text', async () => {
-  render(<App />)
-
-  const helloWorldTextElement = await screen.findByText(/Hello World!/i)
-
-  expect(helloWorldTextElement).toBeInTheDocument()
-})
-
 describe("Pokemon list", () => {
   it("has a list of pokemons including Bulbasaur", async() => {
     render(<App />)
--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <>
-      Hello World!
       <ul>
         <li>Bulbasaur</li>
         <li>Pikachu</li>
         <li>Charmander</li>
       </ul>
-    </>
   );
 }
 
