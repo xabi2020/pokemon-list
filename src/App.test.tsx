b1c1bc0098b62a4cd67c2916d5ee6b3f30f48ef9
xabi2020 <xfuentes@hotmail.com>
2020-10-16 10:32:32 +0200

    Test (en rojo) muestra la lista de todos los pokemons

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import pokemonsData from "./fixtures/pokemons.json"
 
 describe("Pokemon list", () => {
   it("has a list of pokemons including Bulbasaur", async() => {
     render(<App />)
     const bulbasaurTextElement = await screen.findByText(/Bulbasaur/i)
     expect(bulbasaurTextElement).toBeInTheDocument();
+  })
+
+  it("has a list of all the pokemons", async() => {
+    render(<App/>)
+    for (let pokemon of pokemonsData.results) {
+      const pokemonTextElement = await screen.findByText(pokemon.name)
+      expect(pokemonTextElement).toBeInTheDocument();
+    }
   })
 })
